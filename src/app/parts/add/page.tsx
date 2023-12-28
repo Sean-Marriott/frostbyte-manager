b4dff68032f6cf6a3dbe9c2fdb6f9a4c3af7dca5
Sean <sma462@uclive.ac.nz>
2023-12-29 10:21:56 +1300

    Prototyping parts page layout

--- src/app/parts/add/page.tsx
+++ src/app/parts/add/page.tsx
@@ -1,9 +1,16 @@
 import PartForm from "@/components/PartForm";
+import { Grid, Box } from "@mui/material";
 
 export default function partsPage() {
-    return (
-        <div>
-            <PartForm />
-        </div>
-    )
+  return (
+    <Grid container spacing={1} justifyContent="center">
+      <Grid item xs={7}>
+        <h1>Parts Table</h1>
+        <Box sx={{ bgcolor: "#cfe8fc", height: "100vh" }} />
+      </Grid>
+      <Grid item xs={5}>
+        <PartForm />
+      </Grid>
+    </Grid>
+  );
 }
--- src/components/PartForm.tsx
+++ src/components/PartForm.tsx
@@ -3,12 +3,7 @@ import StyledPaper from "./StyledPaper";
 import ConditionSelect from "./ConditionSelect";
 import StatusSelect from "./StatusSelect";
 import PurchaseDateSelector from "./PurchaseDateSelector";
-import {
-  Button,
-  InputAdornment,
-  Stack,
-  TextField,
-} from "@mui/material";
+import { Button, InputAdornment, Stack, TextField } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
 import { FormProvider, useForm } from "react-hook-form";
 import dayjs, { Dayjs } from "dayjs";
@@ -24,7 +19,7 @@ export type FormValues = {
   status: string;
 };
 
-const pb = new PocketBase('http://127.0.0.1:8090');
+const pb = new PocketBase("http://127.0.0.1:8090");
 
 const PartForm = () => {
   const form = useForm<FormValues>({
@@ -41,16 +36,14 @@ const PartForm = () => {
   const { errors } = formState;
 
   const onSubmit = async (data: FormValues) => {
-    const record = await pb.collection('parts').create((
-      {
-        name: data.name,
-        description: data.description,
-        price: data.price,
-        purchase_date: data.purchaseDate.toISOString(),
-        condition: data.condition,
-        status: data.status
-      }
-    ));
+    const record = await pb.collection("parts").create({
+      name: data.name,
+      description: data.description,
+      price: data.price,
+      purchase_date: data.purchaseDate.toISOString(),
+      condition: data.condition,
+      status: data.status,
+    });
   };
 
   return (
@@ -62,62 +55,66 @@ const PartForm = () => {
         <Grid>
           <StyledPaper sx={{ borderRadius: 6 }}>
             <FormProvider {...form}>
-            <form onSubmit={handleSubmit(onSubmit)} noValidate>
-              <Grid container spacing={3} p={1}>
-                <Grid xs={12} sm={6}>
-                  <TextField
-                    label="Name"
-                    fullWidth
-                    {...register("name", {
-                      required: "Name is required",
-                    })}
-                    error={!!errors.name}
-                    helperText={errors.name?.message}
-                  />
+              <form onSubmit={handleSubmit(onSubmit)} noValidate>
+                <Grid container spacing={3} p={1}>
+                  <Grid xs={12} sm={6}>
+                    <TextField
+                      label="Name"
+                      fullWidth
+                      {...register("name", {
+                        required: "Name is required",
+                      })}
+                      error={!!errors.name}
+                      helperText={errors.name?.message}
+                    />
+                  </Grid>
+                  <Grid xs={12} sm={6}>
+                    <TextField
+                      label="Description"
+                      fullWidth
+                      {...register("description", {
+                        required: "Description is required",
+                      })}
+                      error={!!errors.description}
+                      helperText={errors.description?.message}
+                    />
+                  </Grid>
+                  <Grid xs={12} sm={6}>
+                    <TextField
+                      fullWidth
+                      type="number"
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">$</InputAdornment>
+                        ),
+                      }}
+                      {...register("price", {
+                        required: "Price is required",
+                      })}
+                      error={!!errors.price}
+                      helperText={errors.price?.message}
+                    />
+                  </Grid>
+                  <Grid xs={12} sm={6}>
+                    <PurchaseDateSelector />
+                  </Grid>
+                  <Grid xs={12} sm={6}>
+                    <ConditionSelect />
+                  </Grid>
+                  <Grid xs={12} sm={6}>
+                    <StatusSelect />
+                  </Grid>
+                  <Stack
+                    direction={"row-reverse"}
+                    sx={{ width: "100%" }}
+                    pr={1}
+                  >
+                    <Button type="submit" variant="outlined">
+                      Submit
+                    </Button>
+                  </Stack>
                 </Grid>
-                <Grid xs={12} sm={6}>
-                  <TextField
-                    label="Description"
-                    fullWidth
-                    {...register("description", {
-                      required: "Description is required",
-                    })}
-                    error={!!errors.description}
-                    helperText={errors.description?.message}
-                  />
-                </Grid>
-                <Grid xs={12} sm={6}>
-                  <TextField
-                    fullWidth
-					type="number"
-                    InputProps={{
-                      startAdornment: (
-                        <InputAdornment position="start">$</InputAdornment>
-                      ),
-                    }}
-                    {...register("price", {
-                      required: "Price is required",
-                    })}
-                    error={!!errors.price}
-                    helperText={errors.price?.message}
-                  />
-                </Grid>
-                <Grid xs={12} sm={6}>
-                  <PurchaseDateSelector />
-                </Grid>
-                <Grid xs={12} sm={6}>
-                  <ConditionSelect />
-                </Grid>
-                <Grid xs={12} sm={6}>
-                  <StatusSelect />
-                </Grid>
-                <Stack direction={"row-reverse"} sx={{ width: "100%" }} pr={1}>
-                  <Button type="submit" variant="outlined">
-                    Submit
-                  </Button>
-                </Stack>
-              </Grid>
-            </form>
+              </form>
             </FormProvider>
           </StyledPaper>
         </Grid>
